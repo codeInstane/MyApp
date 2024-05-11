9f0425e9ee8fc80a003afaae2361a1e4876e7ae6
codeInstane <fabian.savill.2020@gmail.com>
2024-05-11 20:20:43 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb',
+  parser: '@typescript-eslint/parser',
+  Plugins: ['@typescript-eslint','react'],
+  rules: {
+    semi: 'off' // 句尾不加分號
+  }
+};
